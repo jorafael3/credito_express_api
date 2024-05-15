dec4f73de058904ac3f4910dac6bfad13e7af491
jorafael3 <jalvaradoe3@gmail.com>
2024-05-15 14:28:15 -0500

    xcv

--- conexion.php
+++ conexion.php
@@ -1,14 +1,14 @@
 <?php
 
-// $sql_serverName = "localhost";
-// $sql_database = "crediweb";
-// $sql_user = "root";
-// $sql_pwd = "";
+$sql_serverName = "localhost";
+$sql_database = "crediweb";
+$sql_user = "root";
+$sql_pwd = "";
 
-$sql_serverName = "50.87.184.179";
-$sql_database = "wsoqajmy_crediweb";
-$sql_user = "wsoqajmy_jorge";
-$sql_pwd = "Equilivre3*";
+// $sql_serverName = "50.87.184.179";
+// $sql_database = "wsoqajmy_crediweb";
+// $sql_user = "wsoqajmy_jorge";
+// $sql_pwd = "Equilivre3*";
 
 
 try {
