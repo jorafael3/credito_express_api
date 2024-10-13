30500646b43cb0b08cc0c8e42ab3a7ffb0715713
jorafael3 <jalvaradoe3@gmail.com>
2024-10-13 11:02:08 -0500

    asd

---
+++ demoganga.php
@@ -0,0 +1,14 @@
+<?php
+
+
+if (isset($_GET["id_unico"]) && isset($_GET["key"])) {
+    $IDUNICO = $_GET["id_unico"];
+    $key = $_GET["key"];
+    if ($key == "12qwd12dqwd6165asdq88") {
+        if($IDUNICO != ""){
+
+        }
+    }
+} else {
+    die();
+}
